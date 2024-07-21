c56b223e239cde4c0a0c6843a5568cef5454e10f
Raisa123 <raya.badalova@gmail.com>
2024-07-21 17:39:23 +0400

    First commit

---
+++ RB.TBC.FinalProjectAPI/Controllers/SmtpController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RB.TBC.FinalProjectAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SmtpController : ControllerBase
+    {
+
+
+        [HttpGet]
+        public IActionResult GetDetails()
+        {
+            return Ok("Hello");
+        }
+
+        [HttpGet]
+        [Route("/{id:int}")]
+        public IActionResult GetDetailById(int id)
+        {
+            return Ok(id);
+        }
+    }
+}
